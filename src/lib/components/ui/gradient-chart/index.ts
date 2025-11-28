6ec496c0469978a20ffe73fd210ca0d180904a20
Steven Fitzpatrick <steven@fitz.gg>
2025-11-28 13:50:35 +0000

    lint

--- src/lib/components/ui/gradient-chart/index.ts
+++ src/lib/components/ui/gradient-chart/index.ts
@@ -1,4 +1,7 @@
 import GradientChart from "./gradient-chart.svelte";
-import type { TimeSeriesConfig, TimeSeriesDataPoint } from "./gradient-chart.svelte";
+import type {
+  TimeSeriesConfig,
+  TimeSeriesDataPoint,
+} from "./gradient-chart.svelte";
 
 export { GradientChart, type TimeSeriesConfig, type TimeSeriesDataPoint };
