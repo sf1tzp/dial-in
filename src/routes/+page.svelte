3cdf1d03f2570cc5661aa71a16221aaaf63ffff5
Steven Fitzpatrick <steven@fitz.gg>
2025-11-28 05:17:10 +0000

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+    import { VList } from "virtua/svelte";
+</script>
+
 <h1>Dial In</h1>
 
 <div>
+<Button>Coffee</Button>
+<Button>Shot</Button>
+</div>
 
+<div>
+    <p>Timeline Placeholder</p>
 </div>
 
