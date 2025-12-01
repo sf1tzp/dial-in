70b5d03b2781459422ed74e86518209934e06273
Steven Fitzpatrick <steven@fitz.gg>
2025-12-01 11:35:48 +0000

    update brew entry styling

--- src/lib/components/timeline/BrewEntry.svelte
+++ src/lib/components/timeline/BrewEntry.svelte
@@ -35,51 +35,43 @@
 	}
 </script>
 
-<div
-	class="p-4 cursor-pointer"
-	onclick={() => detailDialogOpen = true}
-	onkeydown={(e) => e.key === 'Enter' && (detailDialogOpen = true)}
-	role="button"
-	tabindex="0"
->
-	<div class="mb-2 flex items-start justify-between gap-2">
-		<div class="flex flex-wrap items-center gap-2">
-			<Badge
-				variant="secondary"
-				class="bg-amber-100 text-amber-800 dark:bg-amber-900 dark:text-amber-200"
-			>
-				<Coffee class="size-2" />
-				Coffee Brewed
-			</Badge>
-		</div>
+<div class="flex">
+	<div class="my-4 ml-2 mr-4">
+		<Coffee class="size-18 text-muted" />
+
 	</div>
 
-	{#if coffeeBag}
+	<div
+		class="mt-3 cursor-pointer flex flex-col"
+		onclick={() => detailDialogOpen = true}
+		onkeydown={(e) => e.key === 'Enter' && (detailDialogOpen = true)}
+		role="button"
+		tabindex="0"
+	>
 		<!-- interpolate a name based on the createdDate "Saturday Morning Brew" -->
-		<div class="flex justify-between my-4">
-			<h3 class="text-foreground font-semibold">
-				{coffeeBag.name}
-			</h3>
-			<span class="pt-2 text-muted-foreground text-end text-xs align-baseline ">
-				{relativeTime}
-			</span>
-		</div>
-	{/if}
+		<h3 class="text-foreground font-semibold">
+			Saturday Morning Brew
+		</h3>
+		<h3 class="text-muted-foreground text-sm">
+			{coffeeBag?.name}
+		</h3>
 
-	<div class="mt-4 text-muted-foreground flex text-sm justify-between">
-		<div class="m-2">
-			<Cog class="size-4 text-foreground text-bold inline-block align-text-top  mr-2 "/>
-			{coffeeBrew?.grindSetting}
-		</div>
-		<div class="m-2">
-			<Weight class="size-4 text-foreground text-bold align-text-top inline-block mr-2"/>
-			{coffeeBrew?.dryWeight}g
-		</div>
-		<div class="m-2">
-			<Gauge class="size-4 text-foreground text-bold align-text-top inline-block mr-2"/>
-			{coffeeBrew?.pressureReading}%
+		<div class="text-center flex justify-between gap-4 my-2">
+			<div class="flex gap-2">
+				<Cog class="size-6 text-muted-foreground"/>
+				<p>{coffeeBrew?.grindSetting.toFixed(1)}</p>
+			</div>
+			<div class="flex gap-2">
+				<Weight class="size-6 text-muted-foreground"/>
+				<p>{coffeeBrew?.dryWeight}g </p>
+			</div>
+			<div class="flex gap-2">
+				<Gauge class="size-6 text-muted-foreground"/>
+				<p>{coffeeBrew?.pressureReading}%</p>
+			</div>
 		</div>
 	</div>
+
 </div>
 
 {#if coffeeBrew}
