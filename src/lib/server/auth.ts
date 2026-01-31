ff729e2d56ffb85bc5cce50757a773d6ce5c5d0e
Steven Fitzpatrick <steven@fitz.gg>
2026-01-31 01:00:58 +0000

    fix server side auth module

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -4,18 +4,8 @@ import { env as publicEnv } from "$env/dynamic/public";
 const PUBLIC_BETTER_AUTH_URL = publicEnv.PUBLIC_BETTER_AUTH_URL || "auth.staging.lofi";
 
 import { env } from "$env/dynamic/private";
-const COOKIE_DOMAIN = env.COOKIE_DOMAIN || ".staging.lofi";
 
 export const auth = betterAuth({
   baseURL: PUBLIC_BETTER_AUTH_URL,
-  fetchOptions: {
-    credentials: "include", // Required for cross-origin cookies
-  },
-  advanced: {
-    crossSubDomainCookies: {
-      domain: COOKIE_DOMAIN,
-      enabled: true,
-    },
-  },
-  trustedOrigins: [PUBLIC_BETTER_AUTH_URL]
+  secret: env.BETTER_AUTH_SECRET
 });
